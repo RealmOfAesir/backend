62c01d9c9017eb8f2a4a5615aeab90dfd79864d3
Michael de Lang <kingoipo@gmail.com>
2017-05-07 22:13:36 +0200

    Split sql into main and world

--- init_aesir_main.sql
+++ init_aesir_main.sql
@@ -1,36 +1,3 @@
-CREATE TABLE base_stats (
-    id BIGSERIAL PRIMARY KEY,
-    str INTEGER NOT NULL,
-    agi INTEGER NOT NULL,
-    wis INTEGER NOT NULL,
-    hp INTEGER NOT NULL,
-    exp_curve INTEGER NOT NULL,
-    max_level INTEGER NOT NULL
-);
-
-CREATE TABLE stats (
-    id BIGSERIAL PRIMARY KEY,
-    str INTEGER NOT NULL,
-    agi INTEGER NOT NULL,
-    wis INTEGER NOT NULL,
-    hp INTEGER NOT NULL,
-    exp INTEGER NOT NULL,
-    level INTEGER NOT NULL
-);
-
-CREATE TABLE locations (
-    id BIGSERIAL PRIMARY KEY,
-    zone_id BIGINT NOT NULL,
-    x INTEGER NOT NULL,
-    y INTEGER NOT NULL
-);
-
-CREATE TABLE zones (
-    id BIGSERIAL PRIMARY KEY,
-    "name" text NOT NULL,
-    data text NOT NULL
-);
-
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     username citext NOT NULL,
@@ -39,34 +6,6 @@ CREATE TABLE users (
     login_attempts SMALLINT NOT NULL DEFAULT 0,
     verification_code text DEFAULT NULL,
     admin SMALLINT NOT NULL DEFAULT 0
-);
-
-CREATE TABLE players (
-    id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    base_stats_id BIGINT NOT NULL,
-    stats_id BIGINT NOT NULL,
-    "name" citext NOT NULL
-);
-
-CREATE TABLE items (
-    id BIGSERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    base_stats_id BIGINT NOT NULL,
-    stats_id BIGINT NOT NULL,
-    "name" citext NOT NULL
-);
-
-CREATE TABLE gods (
-    id BIGSERIAL PRIMARY KEY,
-    "name" text NOT NULL
-);
-
-CREATE TABLE monsters (
-    id BIGSERIAL PRIMARY KEY,
-    location_id BIGINT NOT NULL,
-    stats_id BIGINT NOT NULL,
-    "name" citext NOT NULL
 );
 
 CREATE TABLE licenses (
@@ -79,18 +18,13 @@ CREATE TABLE licenses (
 CREATE TABLE banned_users (
     id BIGSERIAL PRIMARY KEY,
     ip text NULL,
-    user_id BIGINT NOT NULL
+    user_id BIGINT NULL
+);
+
+CREATE TABLE settings (
+    "name" text NOT NULL,
+    value text NOT NULL
 );
 
 ALTER TABLE users ADD CONSTRAINT "users_username_unique" UNIQUE (username);
-
-ALTER TABLE locations ADD CONSTRAINT "locations_map_id_fkey" FOREIGN KEY (zone_id) REFERENCES zones(id);
-ALTER TABLE players ADD CONSTRAINT "players_base_stats_id_fkey" FOREIGN KEY (base_stats_id) REFERENCES base_stats(id);
-ALTER TABLE players ADD CONSTRAINT "players_stats_id_fkey" FOREIGN KEY (stats_id) REFERENCES stats(id);
-ALTER TABLE players ADD CONSTRAINT "players_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE items ADD CONSTRAINT "items_base_stats_id_fkey" FOREIGN KEY (base_stats_id) REFERENCES base_stats(id);
-ALTER TABLE items ADD CONSTRAINT "items_stats_id_fkey" FOREIGN KEY (stats_id) REFERENCES stats(id);
-ALTER TABLE items ADD CONSTRAINT "items_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE monsters ADD CONSTRAINT "monsters_location_id_fkey" FOREIGN KEY (location_id) REFERENCES locations(id);
-ALTER TABLE monsters ADD CONSTRAINT "monsters_stats_id_fkey" FOREIGN KEY (stats_id) REFERENCES stats(id);
 ALTER TABLE banned_users ADD CONSTRAINT "banned_users_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id);
--- seed.sql
+++ seed.sql
@@ -1,7 +1,9 @@
 INSERT INTO licenses ("name", author, license) VALUES ('http://pousse.rapiere.free.fr/tome/tome-tiles.htm',  'David Gervais', 'https://creativecommons.org/licenses/by/3.0/');
 
-INSERT INTO gods ("name") VALUES ('Andhrimnir');
-INSERT INTO gods ("name") VALUES ('Loki');
-INSERT INTO gods ("name") VALUES ('Odin');
-INSERT INTO gods ("name") VALUES ('Thor');
-INSERT INTO gods ("name") VALUES ('Freya');
+--INSERT INTO gods ("name") VALUES ('Andhrimnir');
+--INSERT INTO gods ("name") VALUES ('Loki');
+--INSERT INTO gods ("name") VALUES ('Odin');
+--INSERT INTO gods ("name") VALUES ('Thor');
+--INSERT INTO gods ("name") VALUES ('Freya');
+
+INSERT INTO settings ("name", value) VALUES ('maintenance_mode', '0');
