c25f9b7574b598f2c718134580966001650bab7a
Michael de Lang <kingoipo@gmail.com>
2017-05-25 23:11:38 +0200

    Fix bash substition error

--- compile_external.sh
+++ compile_external.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CPUS="$( grep -c ^processor /proc/cpuinfo )"
 cd $DIR/external/common_backend
-sh ./compile_external.sh
+./compile_external.sh
 cd $DIR/external/libsodium
 ./configure
 make -j$CPUS
