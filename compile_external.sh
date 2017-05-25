b2cb44867df398f8976d0d5b87db0b291916d38e
Michael de Lang <kingoipo@gmail.com>
2017-05-25 23:17:59 +0200

    Actually compile all dependencies

--- compile_external.sh
+++ compile_external.sh
@@ -3,6 +3,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CPUS="$( grep -c ^processor /proc/cpuinfo )"
 cd $DIR/external/common_backend
 ./compile_external.sh
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo .
+make -j$CPUS
 cd $DIR/external/libsodium
 ./configure
 make -j$CPUS
